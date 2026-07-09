a447b8240a8c1df5332218be8e22a845a7a548a2
Demian <dem510rm1e@gmail.com>
2026-07-09 17:23:40 -0600

    Se agrega estructura inicial del documento en LaTeX y gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,9 @@ plugins {
 android {
     namespace = "com.example.ecolizer"
     compileSdk {
-        version = release(36)
+        version = release(36) {
+            minorApiLevel = 1
+        }
     }
 
     buildFeatures {
