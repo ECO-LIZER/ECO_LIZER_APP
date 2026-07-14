ed1e620771ea37c2fe572850316ae1db944a80ce
Kevin Abinady Mejia Flores <kevin.mejiaf@coppel.com>
2026-07-14 03:01:17 -0600

    Lógica de las vistas
    Base activity
    Main Activity
    Full Contents
    maintenance
    ObjectIncompatible

--- app/src/main/kotlin/com/example/ecolizer/activities/BaseActivity.kt
+++ app/src/main/kotlin/com/example/ecolizer/activities/BaseActivity.kt
@@ -1,33 +1,64 @@
 package com.example.ecolizer.activities
 
+import android.app.AlertDialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import com.example.ecolizer.R
 
 abstract class BaseActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        hideBars()
+        ocultarBarras()
     }
 
-    // Kevin Abinady Mejia Flores
     override fun setContentView(view: View?) {
         super.setContentView(view)
     }
-    //Demian Ram Mer
+
     override fun onDestroy() {
         super.onDestroy()
     }
-    // Manuel
-    fun hideBars() {
+
+    // Demian
+    fun ocultarBarras() {
         supportActionBar?.hide()
 
         val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
         windowInsetsController.systemBarsBehavior =
             WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+    }
+
+    // manuel
+    protected fun configurarBotonInfo() {
+        val btnInfo = findViewById<View>(R.id.btnInfo)
+
+        btnInfo?.setOnClickListener {
+            mostrarGuiaReciclaje()
+        }
+    }
+
+    // victor
+    private fun mostrarGuiaReciclaje() {
+        val builder = AlertDialog.Builder(this)
+        val view = layoutInflater.inflate(R.layout.activity_guide, null)
+
+        builder.setView(view)
+        val dialog = builder.create()
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val btnClose = view.findViewById<Button>(R.id.btnCloseInfo)
+        btnClose.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 }
